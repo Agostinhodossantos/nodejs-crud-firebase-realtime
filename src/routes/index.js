c5f9ddc2f752604a074c286d91d4f0c52e6f0aee
AgostinhoDossantos <agostinhodossantos27@gmail.com>
2020-08-24 04:18:35 -0700

    Connect to firebase

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,9 @@ router.get('/' , (req , res)=>{
     res.render('index');
 });
 
+router.post('/new-contact', (req , res) => {
+    console.log(req.body);
+    res.send('recebido');
+});
+
 module.exports = router;
