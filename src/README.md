ad77b9794fa2300646735f019951ba5decc008dc
AgostinhoDossantos <agostinhodossantos27@gmail.com>
2020-08-25 02:05:10 -0700

    crud operation

---
+++ src/README.md
@@ -0,0 +1,1 @@
+$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\ONEMEDIA4\Desktop\firebase-example\apptraining-4e270-firebase-adminsdk-ezwla-ccab79d1f9.json"
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,41 @@
 const { Router} = require('express');
 const router = Router();
+const admin = require('firebase-admin');
+
+//Firebase initializeApp
+//https://www.youtube.com/watch?v=b6KJ7FSMifw
+var serviceAccount = require("../../apptraining-4e270-firebase-adminsdk-ezwla-ccab79d1f9.json");
+
+admin.initializeApp({
+  
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://apptraining-4e270.firebaseio.com/'
+});
+
+const db = admin.database();
 
 router.get('/' , (req , res)=>{
-    res.render('index');
+    db.ref('contacts').once('value' , (snapshot) => {
+       const data = snapshot.val();
+       res.render('index', {contacts: data });
+    });
+   
 });
 
 router.post('/new-contact', (req , res) => {
-    console.log(req.body);
-    res.send('recebido');
+    const newContact = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        phone: req.body.phone
+    };
+    db.ref('contacts').push(newContact);
+    res.redirect('/');
+});
+
+router.get('/delete-contact/:id' , (req , res) => {
+    db.ref('contacts/' + req.params.id).remove();
+    res.redirect('/');
 });
 
 module.exports = router;
